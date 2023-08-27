1910f148fdf3e7f0fd6d86236d49689b9d8810a1
ahmad244 <ahmad2441999@hotmail.com>
2023-08-27 22:43:09 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Chat-gpt/precache-manifest.a438f8c4665e26750b750460449dd6aa.js"
+  "/Chat-gpt/precache-manifest.ddacadc7631b91fb987070c33d8b73e1.js"
 );
 
 workbox.clientsClaim();
